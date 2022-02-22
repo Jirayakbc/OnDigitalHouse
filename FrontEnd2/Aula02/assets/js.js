78a2390864b1544c8d3c464bf9370921ca23dc14
Jirayakbc <feliperobertorocha@gmail.com>
2022-02-22 15:48:57 -0300

    FrontEND2

---
+++ FrontEnd2/Aula02/assets/js.js
@@ -0,0 +1,72 @@
+//jogada da maquina automatizada
+
+
+
+switch () {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+
+
+
+/*let jogadas = 0;
+let pont1 = 0;
+let pont2 = 0;
+
+
+
+
+
+/*
+while (jogadas <3){
+    let maquina = parseInt(Math.random()*3+1)
+    let jogada = prompt("Escolha 1-Pedra, 2- Tesoura ou 3- papel")
+
+    if (maquina == 1 && jogada == 1){
+        console.log("Empate, ambos escolheram pedra")
+        pont1 = pont1 +1
+        pont2 = pont2 +1
+        jogadas = jogadas +1
+    }if (maquina == 2 && jogada == 2){
+        console.log("Empate, ambos escolheram tesoura")
+        pont1 = pont1 +1
+        pont2 = pont2 +1
+        jogadas = jogadas +1
+    }if (maquina == 3 && jogada == 3){
+        console.log("Empate, ambos escolheram papel")
+        pont1 = pont1 +1
+        pont2 = pont2 +1
+        jogadas = jogadas +1
+    }if (maquina == 1 && jogada == 2){
+        console.log("Máquina escolheu 1-Pedra e você 2-Tesoura")
+        pont1 = pont1 +1
+        jogadas = jogadas +1
+    }if (maquina ==1 && jogada ==3){
+        console.log("Máquina escolheu 1-Pedra e você  3-Papel")
+        pont2 = pont2 +1
+        jogadas = jogadas +1
+    }if (maquina ==2 && jogada ==1){
+        console.log("Máquina escolheu 2-Tesoura e você 1-Pedra")
+        pont2 = pont2 +1
+        jogadas = jogadas +1
+    }
+
+}
+
+    
+
+
+
+
+
+
+
+
+   
+
