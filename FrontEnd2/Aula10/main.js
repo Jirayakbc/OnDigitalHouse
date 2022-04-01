ae3a30151b90d092847f0a3b4cd79a86232a13d2
Jirayakbc <feliperobertorocha@gmail.com>
2022-04-01 14:33:48 -0300

    arquivo aula10

---
+++ FrontEnd2/Aula10/main.js
@@ -0,0 +1,9 @@
+let inputNomeRef = document.querySelector('#inputNome')
+let inputTermosRef = document.querySelector('#inputTermos')
+let botaoConcluirCadastroReferencia = document.querySelector('#botaoConcluirCadastro')
+
+
+botaoConcluirCadastroReferencia.addEventListener('click', function(event){
+    
+        event.preventDefault()
+})
