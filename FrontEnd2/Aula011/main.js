a4cd49fe9c9322e4d1bcf9668315645dc1fb0425
Jirayakbc <feliperobertorocha@gmail.com>
2022-04-28 16:48:46 -0300

    Aula011

---
+++ FrontEnd2/Aula011/main.js
@@ -0,0 +1,14 @@
+let botaoLoginReferencia = document.querySelector('#botaoLogin');
+let inputEmailReferencia = document.querySelector('#inputEmail')
+
+
+inputEmailReferencia.addEventListener('keyup', function(event){
+   if (inputEmailReferencia.value ===''){
+    inputEmailReferencia.innerHTML()
+   }
+})
+
+botaoLoginReferencia.addEventListener('click', function(event){
+    event.preventDefault();
+
+})
